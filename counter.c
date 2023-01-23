f04438e62460a17e95435e10ef19b35830003a2f
George <fed.georg @ yandex dot ru>
2023-01-23 22:40:19 +0300

    rename errors

--- counter.c
+++ counter.c
@@ -2,7 +2,7 @@
 
 #include "counter.h"
 
-static const counter_res_t error_empty_counter = {
+static const counter_res_t counter_error_empty = {
 	.value      = 0,
 
 	.error.has  = 1,
@@ -10,7 +10,7 @@ static const counter_res_t error_empty_counter = {
 	.error.info = "the empty counter pointer was passed"
 };
 
-static const counter_res_t error_overflow_counter = {
+static const counter_res_t counter_error_overflow = {
 	.value      = -1,
 
 	.error.has  = 1,
@@ -22,7 +22,7 @@ counter_res_t
 counter_init(counter_t *c)
 {
 	if (c == NULL) {
-		return error_empty_counter;
+		return counter_error_empty;
 	}
 
 	c->acc = 0;
@@ -37,13 +37,13 @@ counter_res_t
 counter_inc(counter_t *c)
 {
 	if (c == NULL) {
-		return error_empty_counter;
+		return counter_error_empty;
 	}
 
 	c->acc++;
 
 	if (c->acc < 0) {
-		return error_overflow_counter;
+		return counter_error_overflow;
 	}
 
 	return (counter_res_t) {
@@ -56,7 +56,7 @@ counter_res_t
 counter_amount(counter_t *c)
 {
 	if (c == NULL) {
-		return error_empty_counter;
+		return counter_error_empty;
 	}
 
 	return (counter_res_t) {
@@ -69,7 +69,7 @@ counter_res_t
 counter_count_to(counter_t *c, size_t limit)
 {
 	if (c == NULL) {
-		return error_empty_counter;
+		return counter_error_empty;
 	}
 
 	for (size_t i = 0; i < limit; i++) {
@@ -77,7 +77,7 @@ counter_count_to(counter_t *c, size_t limit)
 		res = counter_inc(c);
 		if (res.error.has) {
 			if (res.error.type == OVERFLOW) {
-				return error_overflow_counter;
+				return counter_error_overflow;
 			}
 		}
 	}
@@ -93,7 +93,7 @@ counter_res_t
 counter_reset(counter_t *c)
 {
 	if (c == NULL) {
-		return error_empty_counter;
+		return counter_error_empty;
 	}
 
 	c->acc = 0;
@@ -103,14 +103,4 @@ counter_reset(counter_t *c)
 		.error.has = 0
 	};
 }
-
-
-
-
-
-
-
-
-
-
 
